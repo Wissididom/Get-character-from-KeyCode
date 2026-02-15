3835d46ab294aa8da798f6b2e7616d24d5eaa500
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-02-15 20:50:01 +0100

    Initial commit

---
+++ Get character from KeyCode/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_OK = New System.Windows.Forms.Button()
+        Me.nud_KeyCode = New System.Windows.Forms.NumericUpDown()
+        CType(Me.nud_KeyCode, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn_OK
+        '
+        Me.btn_OK.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_OK.Location = New System.Drawing.Point(256, 9)
+        Me.btn_OK.Name = "btn_OK"
+        Me.btn_OK.Size = New System.Drawing.Size(209, 116)
+        Me.btn_OK.TabIndex = 0
+        Me.btn_OK.Text = "Get"
+        Me.btn_OK.UseVisualStyleBackColor = True
+        '
+        'nud_KeyCode
+        '
+        Me.nud_KeyCode.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud_KeyCode.Location = New System.Drawing.Point(12, 9)
+        Me.nud_KeyCode.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.nud_KeyCode.Name = "nud_KeyCode"
+        Me.nud_KeyCode.Size = New System.Drawing.Size(238, 116)
+        Me.nud_KeyCode.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 131)
+        Me.Controls.Add(Me.nud_KeyCode)
+        Me.Controls.Add(Me.btn_OK)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Get character from KeyCode"
+        CType(Me.nud_KeyCode, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btn_OK As Button
+    Friend WithEvents nud_KeyCode As NumericUpDown
+End Class
---
+++ Get character from KeyCode/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub btn_OK_Click(sender As Object, e As EventArgs) Handles btn_OK.Click
+        MsgBox(Chr(nud_KeyCode.Value), 0, "")
+    End Sub
+
+    Private Sub btn_OK_KeyDown(sender As Object, e As KeyEventArgs) Handles btn_OK.KeyDown
+        verentern(sender, e)
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        verentern(sender, e)
+    End Sub
+
+    Private Sub nud_KeyCode_KeyDown(sender As Object, e As KeyEventArgs) Handles nud_KeyCode.KeyDown
+        verentern(sender, e)
+    End Sub
+
+    Private Sub verentern(sender As Object, e As KeyEventArgs)
+        Select Case e.KeyCode
+            Case 13 'Enter
+                btn_OK.PerformClick()
+        End Select
+    End Sub
+End Class
